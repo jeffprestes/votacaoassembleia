93e03b52da7a7e026c659300397df46ebc9635cc
Jeff Prestes <jeffprestes@gmail.com>
2018-10-24 16:27:58 -0300

    Inclusao de secretario e presidente

--- contracts/votacao.sol
+++ contracts/votacao.sol
@@ -17,40 +17,63 @@ contract Assembleia {
         bool existe;
     }
 
-    modifier somenteChairman() {
-        require(chairman == msg.sender, "Só o Chairmain pode realizar essa operação");
+    modifier somenteSecretario() {
+        require(secretario == msg.sender, "Só o Chairmain pode realizar essa operação");
         _;
     }
 
     //Informações gerais da Assembleia
     Proposta[] propostas;
     mapping (address =>Votante) votantes;
-    address chairman;
+    Votante[] numeroVotantes;
+    address secretario;
+    address presidente;
     uint dataInicioVotacao;
     uint dataFimVotacao;
     string motivoConvocatoria;
 
+    /*
+    @qualDataFimVotacao - Unix timestamp - Numero de segundos decorridos desde 1 janeiro de 1970. 
+    Formato usado amplamente no ambiente de programação. Para converter em data atual pode-se usar:
+    https://www.unixtimestamp.com/index.php
+    */
     constructor (string qualMotivoConvocatoria, uint qualDataFimVotacao) public {
-        chairman = msg.sender;
+        presidente = msg.sender;
         dataFimVotacao = qualDataFimVotacao;
         dataInicioVotacao = now;
         motivoConvocatoria = qualMotivoConvocatoria;
     }
 
-    function incluiVotante(address enderecoVotante, uint quotaDeVotos) public somenteChairman {
+    function designarSecretario(address secretarioDesignado) public {
+        require(presidente == msg.sender, "Somente o presidente pode designar o secretario");
+        secretario = secretarioDesignado;
+    }
+
+    function incluiVotante(address enderecoVotante, uint quotaDeVotos) public somenteSecretario {
         require(quotaDeVotos <= 99, "Quota nao pode ser superior a 99%");
         require(enderecoVotante != address(0), "O votante deve ter um endereco valido");
         Votante memory novoVotante = Votante(enderecoVotante, quotaDeVotos, false, true);
         votantes[enderecoVotante] = novoVotante;
+        numeroVotantes.push(novoVotante);
     }
 
-    function incluiProposta(string qualTextoDaProposta, address qualProponente, uint qualQuotaMinimaParaAprovacao) public somenteChairman {
+    function incluiProposta(string qualTextoDaProposta, address qualProponente, uint qualQuotaMinimaParaAprovacao) public somenteSecretario {
         Proposta memory novaProposta = Proposta(qualTextoDaProposta, qualProponente, 0, qualQuotaMinimaParaAprovacao, true);
         propostas.push(novaProposta);
     }
     
     function pesquisarVotante(address indiceVotante) public view returns (address, uint) {
         Votante memory votanteTemporario = votantes[indiceVotante];
+        if (votanteTemporario.existe == true) {
+            return (votanteTemporario.conta, votanteTemporario.quotaDeVotos);
+        } else {
+            return (0,0);
+        }
+    }
+
+    function pesquisarVotantePorIndice(uint indiceVotante) public view returns (address, uint) {
+        require(indiceVotante <= numeroVotantes.length, "Indice informado é maior que o numero de votantes");
+        Votante memory votanteTemporario = numeroVotantes[indiceVotante];
         if (votanteTemporario.existe == true) {
             return (votanteTemporario.conta, votanteTemporario.quotaDeVotos);
         } else {
@@ -71,6 +94,10 @@ contract Assembleia {
         return propostas.length;
     }
 
+    function totalDeVotantes() public view returns (uint) {
+        return numeroVotantes.length;
+    }
+
     function qualMotivoDaConvocatoria() public view returns (string) {
         return motivoConvocatoria;
     }
@@ -88,8 +115,10 @@ contract Assembleia {
         return dataFimVotacao;
     }
 
-    function detalhesAssembleia() public view returns(address, string, uint, uint, uint) {
-        return (chairman, motivoConvocatoria, dataInicioVotacao, dataFimVotacao, totalDePropostas());
+    function detalhesAssembleia() public view returns(address, address, string, uint, uint, uint, uint) {
+        uint tPropostas = totalDePropostas();
+        uint tVotantes = totalDeVotantes();
+        return (presidente, secretario, motivoConvocatoria, dataInicioVotacao, dataFimVotacao, tPropostas, tVotantes);
     }
 
     function votar(uint numeroProposta, bool favoravelAProposta) public returns (bool) {
